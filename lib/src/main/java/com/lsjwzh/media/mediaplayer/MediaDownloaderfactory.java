de540cf8920cccf4b8f3da903a08a57e77fa784c
lsjwzh <lsjwzh@gmail.com>
2015-11-29 23:58:32 +0800

    refactor

--- lib/src/main/java/com/lsjwzh/media/mediaplayer/MediaDownloaderfactory.java
+++
@@ -1,8 +0,0 @@
-package com.lsjwzh.media.mediaplayer;
-
-/**
- * Created by wenye on 15/11/28.
- */
-public interface MediaDownloaderFactory {
-  MediaDownloader createMediaDownloader(String uri);
-}
